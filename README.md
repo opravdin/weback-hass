f8bbef69b5bc0e154a3f0c272f9b7ddfeec559c1
Олег Правдин <opravidn@gmail.com>
2020-04-12 18:42:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# WeBack integration for Home Assistant
+Based on my [package](https://github.com/opravdin/weback-unofficial).  
+This thing is in early development status and currently support just a limited amount of devices - only robot vacuums (probably only without VSLAM camera tracking).
+
+## Configuration
+If you use your phone number as username, type it like +{region}-{number}
+```yaml
+weback:
+  - username: +7-1234567890
+    password: <password>
+```
+
+## Supported devices
+This integration tested only on Neatsvor X500. It supports any device that mentioned as "_CLEAN_ROBOT" in Amazon's API. You can check it this way:  
+```
+pip install weback-unofficial
+```
+```python
+from weback_unofficial.client import WebackApi
+
+client = WebackApi("login", "password")
+devices = client.device_list()
+for device in devices:
+    print(f"Checking device {device['Thing_Name']}")
+    description = client.get_device_description(device["Thing_Name"])
+    print(f"Device type is {description.get('thingTypeName')}")
+```
+
+## Custom commands
+Currently you can publish message to device's MQTT topic.  
+Read more about available messages in [API's repository](https://github.com/opravdin/weback-unofficial)
+```yaml
+entity_id: vacuum.robot_name
+command: any_text_here
+params:
+  working_status: AutoClean
+```
+
+## Known issues
+* Mopping control is not supported for now
+* Library in polling based but I am 100% sure that it could handle MQTT messages from vacuum/weback server but don't know how to implement that :(
---
+++ custom_components/weback/__init__.py
@@ -0,0 +1,70 @@
+"""Support for WeBack devices."""
+from datetime import timedelta
+import logging
+import random
+import string
+
+import voluptuous as vol
+from weback_unofficial.client import WebackApi
+from weback_unofficial.vacuum import CleanRobot
+
+from homeassistant.const import CONF_PASSWORD, CONF_SCAN_INTERVAL, CONF_USERNAME
+from homeassistant.helpers import discovery
+import homeassistant.helpers.config_validation as cv
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "weback"
+SCAN_INTERVAL = timedelta(seconds=60)
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_USERNAME): cv.string,
+                vol.Required(CONF_PASSWORD): cv.string,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+WEBACK_DEVICES = "weback_devices"
+
+SUPPORTED_DEVICES = ["_CLEAN_ROBOT"]
+
+
+def setup(hass, config):
+    """Set up the Weback component."""
+    _LOGGER.debug("Creating new Weback component")
+
+    hass.data[WEBACK_DEVICES] = []
+
+    weback_api = WebackApi(
+        config[DOMAIN].get(CONF_USERNAME), config[DOMAIN].get(CONF_PASSWORD),
+    )
+
+    devices = weback_api.device_list()
+    _LOGGER.debug("Weback devices: %s", devices)
+
+    for device in devices:
+        _LOGGER.info(
+            "Discovered Weback device %s with nickname %s",
+            device["Thing_Name"],
+            device["Thing_Nick_Name"],
+        )
+
+        # Fetching device description to check if this device is supported by platform
+        description = weback_api.get_device_description(device["Thing_Name"])
+        if description.get("thingTypeName") not in SUPPORTED_DEVICES:
+            _LOGGER.info("Device not supported by this integration")
+            continue
+
+        vacuum = CleanRobot(device["Thing_Name"], weback_api, None, description)
+        hass.data[WEBACK_DEVICES].append(vacuum)
+
+    if hass.data[WEBACK_DEVICES]:
+        _LOGGER.debug("Starting vacuum components")
+        discovery.load_platform(hass, "vacuum", DOMAIN, {}, config)
+
+    return True
